89a66ff2016a4c16ac08153aac0ffdf4de665824
Adam Zabloudil <adampzab@gmail.com>
2023-01-26 17:01:27 -0800

    folders added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # icedCoffee
+
 A compiler for the language icedCoffee
+
+## Examples
+
+## About the author
---
+++ src/icedCoffee.js
@@ -0,0 +1,5 @@
+console.log("hello from the icedCoffee compiler!");
+
+export function add(x, y) {
+  return x + y;
+}
