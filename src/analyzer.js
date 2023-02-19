d47f5405e6a7bd39e48319b706e617f181120b6e
Adam Zabloudil <adampzab@gmail.com>
2023-02-18 16:45:35 -0800

    more structure added

---
+++ src/analyzer.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+import ohm from "ohm-js";
+import * as core from "./core.js";
+
+// Throw an error message that takes advantage of Ohm's messaging
+function error(message, node) {
+  if (node) {
+    throw new Error(`${node.source.getLineAndColumnMessage()}${message}`);
+  }
+  throw new Error(message);
+}
+
+const icedCoffeeGrammar = ohm.grammar(fs.readFileSync("src/icedCoffee.ohm"));
+
+export default function analyze(sourceCode) {
+  const match = icedCoffeeGrammar.match(sourceCode);
+  if (!match.succeeded()) error(match.message);
+  console.log("You're good!");
+}
